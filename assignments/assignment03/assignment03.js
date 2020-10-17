c74b4e173138d8b5f881b1e930149531efc2b293
Blue RiverFlow <43072021+dlfegan@users.noreply.github.com>
2020-10-17 01:08:41 -0400

    got parTotal to work, on to scoreTotal and overTotal

--- assignments/assignment03/assignment03.js
+++ assignments/assignment03/assignment03.js
@@ -1,6 +1,7 @@
 let elem = [];
+let totals = [];
 
-// assign the entire table row for hole 1 to a variable, elem
+// assign variables to the elem ID in the html
 elem[1]
   = document.getElementById("1");
 elem[2]
@@ -37,6 +38,8 @@ elem[17]
   = document.getElementById("17");
 elem[18]
   = document.getElementById("18");
+elem[19]  
+  = document.getElementById("totals");
 
 
 // display the number of children (all td elements)
@@ -47,6 +50,7 @@ elem[18]
 // assign a function to the + button
 //Made each button have its own function
 //to add one to its specific hole
+// Could i have done this using a loop?
 elem[1].children[4].children[0].onclick 
   = function(){add1(elem[1]);};
 elem[2].children[4].children[0].onclick 
@@ -84,9 +88,10 @@ elem[17].children[4].children[0].onclick
 elem[18].children[4].children[0].onclick 
   = function(){add1(elem[18]);};
 
+
+
 // assign a function to the - button
-//Made each button have its own function
-//to subtract one to its specific hole
+// Could i have done this using a loop?
 elem[1].children[4].children[1].onclick
   = function(){subtract1(elem[1]);};
 elem[2].children[4].children[1].onclick 
@@ -124,17 +129,21 @@ elem[17].children[4].children[1].onclick
 elem[18].children[4].children[1].onclick 
   = function(){subtract1(elem[18]);};
 
+
 let currentScore=0;
 let over;
+
 //1: addtion to each + button
 // make it so they cannot double par(8)
 // create an "add1" function
 function add1 (elem) {
   let counter = 8;
  
+  // Got it so the score can NOT
+  // go over 8! probably could have done it another way. by elem.children[2].innerHTML isnt double the currentScore.
   if(elem.children[2].innerHTML == "-")
     elem.children[2].innerHTML = "1";
-  else if (currentScore <= 7) {
+  else if (elem.children[2].innerHTML <= 7) {
     currentScore = elem.children[2].innerHTML;
     currentScore = Number.parseInt(currentScore);
     elem.children[2].innerHTML = currentScore + 1;
@@ -143,15 +152,18 @@ function add1 (elem) {
 
     elem.children[2].innerHTML = currentScore;
 
-  }
-// 3: calculate over  
-    elem.children[3].innerHTML = elem.children[2].innerHTML - 4;
+} // END Function add1
+  
+// 3-A: calculate over for + button 
+    elem.children[3].innerHTML = elem.children[2].innerHTML - elem.children[1].innerHTML;
 
 }     
 
 //2: subtract from each - button
-// make it so each score can NOT be negative
+// got it so each score can NOT be negative!
 // create an "subtract1" function
+//Made each button have its own function
+//to subtract one to its specific hole
 function subtract1 (elem) {
   if(elem.children[2].innerHTML == "-", elem.children[2].innerHTML <="0")
     elem.children[2].innerHTML = "0";
@@ -162,34 +174,127 @@ function subtract1 (elem) {
 
   }
 
+ 
   
-// 3: calculate over  
-    elem.children[3].innerHTML = elem.children[2].innerHTML - 4; 
-
   
-// 4: set yellow background for the
-//    over of 0.
-function yellow() {
-  
-}
-
-
-
-}
+// 3: calculate over for - button
+// Maybe should have made a function for this? 
+// and a function for this to put here and 
+// for the + button?  
+    
+  elem.children[3].innerHTML = elem.children[2].innerHTML - elem.children[1].innerHTML; 
 
   
+} // END Function subtract1  
+  
+// 4  : Totals: 
 
-/* your mission: 
+// 4-C: Over: letoverTotal
 
-1. Make all the + buttons add 1 to the score of the hole. Do not allow the score to exceed double the par. 
-2. Make all the - buttons subtract 1 from the score of the hole. Do not allow the score to be negative!
-3. Make the "Over" column display the difference, score - par, of the hole.
-4. Make a table row's background color yellow if and only if that table row has a nonzero score.
-5. Make the HTML table row with id="totals" display appropriate totals. Totals should be computed only for holes that have yellow-highlighted nonzero scores. Like the yellow-highlighted table rows above, the "totals" table row background color must be yellow if and only if it has a nonzero score. 
-6. Add to the Action column a new button, C, which clears the current score for a given hole, and re-sets the table row background color to default.
-7. Replace HTML table row code with JavaScript code that appends table rows. That is, delete the HTML elements with ID's 1 through 18, and write a function that loops 18 times, appending the appropriate DOM elements with ID's 1 through 18.  
-8. Add a "RESET" button which clears all user-entered data. Use addEventListener method to add functionality to the button. See: https://www.w3schools.com/js/js_htmldom_eventlistener.asp. 
-9. Add an "ABOUT" button which displays the message, "Golf Scorecard 1.0. All rights reserved."  Use an arrow function to add the "ABOUT" button to the DOM. See: https://www.w3schools.com/Js/js_arrow_function.asp.
-10. Add a "FONT" button which toggles the font size of the entire app, from/to smaller to/from larger.
-*. Advanced (optional): put circles around scores that are birdies, and squares around scores that are bogeys!
-*/
+// 4-A: Par: let parTotal
+// Create variables for each par
+// and convert innerHTML to an integer
+let par1 = elem[1].children[1].innerHTML;
+par1 = Number.parseInt(par1);
+let par2 = elem[2].children[1].innerHTML;
+par2 = Number.parseInt(par2);
+let par3 = elem[3].children[1].innerHTML;
+par3 = Number.parseInt(par3);
+let par4 = elem[4].children[1].innerHTML;
+par4 = Number.parseInt(par4);
+let par5 = elem[5].children[1].innerHTML;
+par5 = Number.parseInt(par5);
+let par6 = elem[6].children[1].innerHTML;
+par6 = Number.parseInt(par6);
+let par7 = elem[7].children[1].innerHTML;
+par7 = Number.parseInt(par7);
+let par8 = elem[8].children[1].innerHTML;
+par8 = Number.parseInt(par8);
+let par9 = elem[9].children[1].innerHTML;
+par9 = Number.parseInt(par9);
+let par10 = elem[10].children[1].innerHTML;
+par10 = Number.parseInt(par10);
+let par11 = elem[11].children[1].innerHTML;
+par11 = Number.parseInt(par11);
+let par12 = elem[12].children[1].innerHTML;
+par12 = Number.parseInt(par12);
+let par13 = elem[13].children[1].innerHTML;
+par13 = Number.parseInt(par13);
+let par14 = elem[14].children[1].innerHTML;
+par14 = Number.parseInt(par14);
+let par15 = elem[15].children[1].innerHTML;
+par15 = Number.parseInt(par15);
+let par16 = elem[16].children[1].innerHTML;
+par16 = Number.parseInt(par16);
+let par17 = elem[17].children[1].innerHTML;
+par17 = Number.parseInt(par17);
+let par18 = elem[18].children[1].innerHTML;
+par18 = Number.parseInt(par18);
+
+
+// calculation to add all pars for parTotal
+elem[19].children[1].innerHTML = par1 + par2 + par3 + par4 + par5 + par6 + par7 + par8 + par9 + par10 + par11 + par12 + par13 + par14 + par15 + par16 + par17 + par18;
+
+// 4-B: Score: let scoreTotal
+// Create variables for each score
+// and convert innerHTML to an integer
+let score1 = elem[1].children[2].innerHTML;
+score1 = Number.parseInt(score1);
+let score2 = elem[2].children[2].innerHTML;
+score2 = Number.parseInt(score2);
+let score3 = elem[3].children[2].innerHTML;
+score3 = Number.parseInt(score3);
+let score4 = elem[4].children[2].innerHTML;
+score4 = Number.parseInt(score4);
+let score5 = elem[5].children[2].innerHTML;
+score5 = Number.parseInt(score5);
+let score6 = elem[6].children[2].innerHTML;
+score6 = Number.parseInt(score6);
+let score7 = elem[7].children[2].innerHTML;
+score7 = Number.parseInt(score7);
+let score8 = elem[8].children[2].innerHTML;
+score8 = Number.parseInt(score8);
+let score9 = elem[9].children[2].innerHTML;
+score9 = Number.parseInt(score9);
+let score10 = elem[10].children[2].innerHTML;
+score10 = Number.parseInt(score10);
+let score11 = elem[11].children[2].innerHTML;
+score11 = Number.parseInt(score11);
+let score12 = elem[12].children[2].innerHTML;
+score12 = Number.parseInt(score12);
+let score13 = elem[13].children[2].innerHTML;
+score13 = Number.parseInt(score13);
+let score14 = elem[14].children[2].innerHTML;
+score14 = Number.parseInt(score14);
+let score15 = elem[15].children[2].innerHTML;
+score15 = Number.parseInt(score15);
+let score16 = elem[16].children[2].innerHTML;
+score16 = Number.parseInt(score16);
+let score17 = elem[17].children[2].innerHTML;
+score17 = Number.parseInt(score17);
+let score18 = elem[18].children[2].innerHTML;
+score18 = Number.parseInt(score18);
+
+let totalScore = elem[19].children[2].innerHTML;
+totalScore = Number.parseInt(totalScore);
+elem[19].children[2].innerHTML = Number.parseInt(elem[19].children[2].innerHTML)
+elem[19].children[2].innerHTML = score1 + score2 + score3 + score4 + score5 + score6 + score7 + score8 + score9 + score10 + score11 + score12 + score13 + score14 + score15 + score16 + score17 + score18;
+
+//Notes To Self
+//elem.children[0] = hole
+//elem.children[1] = par
+//elem.children[2] = score
+//elem.children[3] = over
+//elem.children[4].children[0] = positive buttons
+//elem.children[4].children[1] = negative buttons
+
+
+
+
+
+
+
+
+
+  
+
